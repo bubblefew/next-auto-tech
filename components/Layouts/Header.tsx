42070dd6cadc3699d263107d5c39425ed5b9a765
bubblefew <bubble.few@gmail.com>
2023-11-09 22:10:24 +0700

    few

--- components/Layouts/Header.tsx
+++ components/Layouts/Header.tsx
@@ -15,113 +15,108 @@ import { signOut } from "@/store/slices/userSlice";
 const drawerWidth = 240;
 
 interface AppBarProps extends MuiAppBarProps {
-    open?: boolean;
+  open?: boolean;
 }
 
 const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== "open",
 })<AppBarProps>(({ theme, open }) => ({
-    zIndex: theme.zIndex.drawer + 1,
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(["width", "margin"], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
     }),
-    ...(open && {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(["width", "margin"], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }),
+  }),
 }));
 
 type HeaderProp = {
-    open: boolean;
-    onDrawerOpen: () => void;
+  open: boolean;
+  onDrawerOpen: () => void;
 };
 
 export default function Header({ open, onDrawerOpen }: HeaderProp) {
-    const [showProfileMenu, setShowProfileMenu] = React.useState(false);
-    const dispatch = useAppDispatch();
+  const [showProfileMenu, setShowProfileMenu] = React.useState(false);
+  const dispatch = useAppDispatch();
 
-    const handleClose = () => {
-        setShowProfileMenu(false);
-    };
+  const handleClose = () => {
+    setShowProfileMenu(false);
+  };
 
-    return (
-        <AppBar position="fixed" open={open}>
-            <Toolbar>
-                <IconButton
-                    color="inherit"
-                    aria-label="open drawer"
-                    onClick={onDrawerOpen}
-                    edge="start"
-                    sx={{
-                        marginRight: 5,
-                        ...(open && { display: "none" }),
-                    }}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" noWrap component="div">
-                    CMStock Workshop with ReactJS - Typescript (TS) V.
-                    {process.env.NEXT_PUBLIC_APP_VERSION}
-                </Typography>
+  return (
+    <AppBar position="fixed" open={open}>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={onDrawerOpen}
+          edge="start"
+          sx={{
+            marginRight: 5,
+            ...(open && { display: "none" }),
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap component="div">
+          Auto Tech V.
+          {process.env.NEXT_PUBLIC_APP_VERSION}
+        </Typography>
 
-                <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          <IconButton
+            size="large"
+            aria-label="show 4 new mails"
+            color="inherit"
+          >
+            <Badge badgeContent={4} color="error">
+              <MailIcon />
+            </Badge>
+          </IconButton>
+          <IconButton
+            size="large"
+            aria-label="show 17 new notifications"
+            color="inherit"
+          >
+            <Badge badgeContent={17} color="error">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-haspopup="true"
+            onClick={() => setShowProfileMenu(!showProfileMenu)}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
 
-                <Typography variant="h6" noWrap component="div" fontWeight="300">
-                    Updated 2022
-                </Typography>
-                <Box sx={{ display: { xs: "none", md: "flex" } }}>
-                    <IconButton
-                        size="large"
-                        aria-label="show 4 new mails"
-                        color="inherit"
-                    >
-                        <Badge badgeContent={4} color="error">
-                            <MailIcon />
-                        </Badge>
-                    </IconButton>
-                    <IconButton
-                        size="large"
-                        aria-label="show 17 new notifications"
-                        color="inherit"
-                    >
-                        <Badge badgeContent={17} color="error">
-                            <NotificationsIcon />
-                        </Badge>
-                    </IconButton>
-                    <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-haspopup="true"
-                        onClick={() => setShowProfileMenu(!showProfileMenu)}
-                        color="inherit"
-                    >
-                        <AccountCircle />
-                    </IconButton>
-
-                    <Menu
-                        anchorOrigin={{
-                            vertical: "top",
-                            horizontal: "right",
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: "top",
-                            horizontal: "right",
-                        }}
-                        open={showProfileMenu}
-                        onClose={handleClose}
-                    >
-
-                        <MenuItem onClick={() => dispatch(signOut())}>Logout</MenuItem>
-                        <MenuItem onClick={handleClose}>My account</MenuItem>
-                    </Menu>
-                </Box>
-            </Toolbar>
-        </AppBar>
-    );
+          <Menu
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={showProfileMenu}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={() => dispatch(signOut())}>Logout</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
--- components/Layouts/Layout.tsx
+++ components/Layouts/Layout.tsx
@@ -1,60 +1,37 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Header from '@/components/Layouts/Header';
-import Menu from '@/components/Layouts/Menu';
+import * as React from "react";
+import { styled, Theme, CSSObject } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Header from "@/components/Layouts/Header";
+import Menu from "@/components/Layouts/Menu";
 
 const drawerWidth = 240;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
-    open?: boolean;
-}>(({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawerWidth}px`,
-    ...(open && {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-    }),
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
 }));
 
-
-
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-}));
-
-
-type LayoutProp = {
-    children: React.ReactNode;
+interface LayoutProps {
+  children: React.ReactNode;
 }
 
-export default function Layout({ children }: LayoutProp) {
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <Header open={open} onDrawerOpen={() => setOpen(true)} />
-            <Menu open={open} onDrawerClose={() => setOpen(false)} />
-            <Main open={open}>
-                <DrawerHeader />
-                {children}
-            </Main>
-        </Box>
-    );
+export default function Layout({ children }: LayoutProps) {
+  const [open, setOpen] = React.useState(true);
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <Header open={open} onDrawerOpen={() => setOpen(true)} />
+      <Menu open={open} onDrawerClose={() => setOpen(false)} />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        {children}
+      </Box>
+    </Box>
+  );
 }
