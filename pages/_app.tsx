cae634bc889c5d8e6c64007a20564f920ca7f752
bubblefew <bubble.few@gmail.com>
2023-12-19 22:54:16 +0700

    home

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,48 @@ import { store } from '@/store/store';
 import { Provider } from 'react-redux';
 import * as React from "react";
 import { getSession } from '@/store/slices/userSlice';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { blue, red } from '@mui/material/colors'
 function MyApp({ Component, pageProps }: AppProps) {
+  const drawerWidth = 240
+  const theme = createTheme({
+    components: {
+      MuiDrawer: {
+        styleOverrides: {
+          paper: {
+            backgroundImage: 'url("/static/img/background_menu.jpg")',
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "bottom",
+            width: drawerWidth
+          }
+        }
+      }
+    },
+    typography: {
+      fontFamily: "Roboto",
+      fontWeightLight: 300,
+      fontWeightRegular: 400,
+      fontWeightMedium: 500,
+      fontWeightBold: 700
+    },
+    spacing: 8,
+    palette: {
+      primary: process.env.NEXT_PUBLIC_IS_PRODUCTION == "0" ? red : blue,
+      background: {
+        default: "#FFF"
+      }
+    }
+  })
   React.useEffect(() => {
     store.dispatch(getSession())
   })
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ThemeProvider>
+
   )
 }
 
